553a4968327d0d00d68be1ff4dc8f76f86615ff1
SY-rabbit <sumyeetsy@gmail.com>
2025-05-12 22:44:56 +0100

    adding ai element to the website

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log(response.data.answer);
   new Typewriter(".poem-text", {
-    strings: "A New Poem is on the way!",
+    strings: response.data.answer,
     autoStart: true,
   });
 }
 
+function newPoem(event) {
+  event.preventDefault();
+  let key = "a2t704ade1b003d643fa9333o6bb8862";
+  let theme = document.querySelector("#theme-input").value;
+  let prompt = `write a poem about ${theme}`;
+  let context =
+    "please limit the poem to 4 lines and follow the inputed theme, apply rhyme and rhythm. separate each line with a <br>. wirte '<strong>~SheCodes AI</strong> at the bottom as writer of the poem";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  console.log(`prompt:${prompt}`);
+  console.log(`context:${context}`);
+  console.log(`apiUrl:${apiUrl}`);
+  axios.get(apiUrl).then(displayPoem);
+}
+
 let poemFormElement = document.querySelector("#poem-form");
-poemFormElement.addEventListener("submit", generatePoem);
+poemFormElement.addEventListener("submit", newPoem);
